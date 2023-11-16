3966fc345c1b3fd2de9a07214cc7e2245463b0bf
Dhruval Gupta <diggupta092@gmail.com>
2023-11-16 12:11:40 +0530

    updated paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 // import React from "react";
-import './styles/App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./styles/App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./Pages/LoginPage";
+import ForgetPassword from "./Pages/ForgetPassword";
+import RegisterPage from "./Pages/RegisterPage";
+import ForgetPasswordVerify from "./Pages/ForgetPasswordVerify";
 import HomePage from "./HomePage";
 import Companies from "./Companies";
-import Courses from './Courses';
-import CoursesFull from './CoursesFull';
-import Jobs from './Jobs';
+import Courses from "./Courses";
+import CoursesFull from "./CoursesFull";
+import Jobs from "./Jobs";
 
-
-export default function App(){
-  return(
+export default function App() {
+  return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
+          <Route path="login" element={<LoginPage />}></Route>
+          <Route path="forgetpassword" element={<ForgetPassword />}></Route>
+          <Route path="register" element={<RegisterPage />}></Route>
+          <Route path="forgetpasswordverify" element={<ForgetPasswordVerify />}></Route>
           <Route path="/Companies" element={<Companies />}></Route>
           <Route path="/Courses" element={<Courses />}></Route>
           <Route path="/CoursesFull" element={<CoursesFull />}></Route>
@@ -21,5 +28,5 @@ export default function App(){
         </Routes>
       </Router>
     </>
-  )
+  );
 }
