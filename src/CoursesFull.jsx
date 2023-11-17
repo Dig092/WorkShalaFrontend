1228fae7b426168cf821e5229e8b373406b49e05
Dhruval Gupta <diggupta092@gmail.com>
2023-11-17 17:45:23 +0530

    added filter

--- src/CoursesFull.jsx
+++ src/CoursesFull.jsx
@@ -8,7 +8,7 @@ import person from "./assets/icons/person.png";
 
 export default function CoursesFull() {
   const navigate = useNavigate();
-  const [posts, setPosts] = useState([]);
+  const [courses, setCourses] = useState([]);
 
   const getData = () => {
     var requestOptions = {
@@ -20,11 +20,18 @@ export default function CoursesFull() {
       redirect: "follow",
     };
 
-    fetch("https://courses-api-hrph.onrender.com/", requestOptions)
+    fetch(
+      "https://courseapi-s0hm.onrender.com/recommend/blockchain",
+      requestOptions
+    )
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        setPosts(data);
+        if (data && Array.isArray(data.courses)) {
+          setCourses(data.courses);
+        } else {
+          setCourses([]);
+        }
       })
       .catch((error) => console.log("error", error));
   };
@@ -35,31 +42,39 @@ export default function CoursesFull() {
 
   return (
     <>
-    <NavBar />
+      <NavBar />
 
-      <div className="w-full px-16 py-8 text-3xl font-semibold bg-[#FFF6F9]" >Courses For You</div>
-        <div className="flex justify-center items-center w-full bg-[#FFF6F9]">
-          <div className="w-3/4 h-full grid grid-cols-4 gap-16 mt-16 mb-16 mr-16 ">
-            {posts.map((post) => (
+      <div className="w-full px-16 py-8 text-3xl font-semibold bg-[#FFF6F9]">
+        Courses For You
+      </div>
+      <div className="flex justify-center items-center w-full bg-[#FFF6F9]">
+        <div className="w-3/4 h-full grid grid-cols-4 gap-16 mt-16 mb-16 mr-16 ">
+          {courses.length > 0 ? (
+            courses.map((course, index) => (
               <div className="flex flex-col items-center  snap-start border rounded-md flex-shrink-0 w-64 h-80 mx-4">
                 <div className="w-64 h-32 bg-[#FFE5ED] rounded-md "></div>
-                <h1 className="text-lg font-semibold m-4">{post.title}</h1>
-                <h1 className="text-xs pl-4 ">{post.description}</h1>
+                <h1 className="text-lg font-semibold m-4">{course}</h1>
+                <h1 className="text-xs pl-4 ">
+                  Learn to design data models, build data warehouses and data
+                  lakes, automate data pipelines, and manage massive datasets.
+                </h1>
                 <div className="w-60 h-0.5 mt-3 bg-[#FFE5ED]"></div>
                 <div className="w-full h-16 px-4 flex items-center justify-between">
-                  <div>{post.type}</div>
+                  <div>Free</div>
                   <div className="flex items-center gap-1">
                     <img className="w-4 h-4" src={star} alt="" />
-                    <h1 className="text-xs">{post.rating}</h1>
+                    <h1 className="text-xs">4.5</h1>
                     <img className="w-4 h-4" src={person} alt="" />
-                    <h1 className="text-xs">{post.reviewcount}</h1>
+                    <h1 className="text-xs">123</h1>
                   </div>
                 </div>
               </div>
-            ))}
-          </div>
+            ))
+          ) : (
+            <div>No Courses available</div>
+          )}
         </div>
-      
+      </div>
 
       <Footer />
     </>
